7265ea96e16f677587c47fac0d2009a94a87c626
gyan <gyan22@gmail.com>
2025-07-24 16:33:32 +0545

    Add GitHub Actions workflow for CI/CD deployment and webcrypto is added changing the repo name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { webcrypto } from 'node:crypto'
 
-// https://vite.dev/config/
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
+
+// Replace with your actual repo name
+const repoName = 'my-portfolio';
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
